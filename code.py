72b35f9091ada02220ebd18f5072487a4cd60b6b
41562 <kanhaprasad546@gmail.com>
2015-05-08 17:06:10 +0700

    Change to templates

--- code.py
+++ code.py
@@ -2,16 +2,17 @@
 import web
 
 """
-TODO:
-1.Validate the update form and give the appropriate messages
-2.Learn sql databsing or figure out if you need one, if you don't just use a csv stored to the server
-3.Once done with the sql, use it to write the Check and Clear classes
-4. For the check class, use mpld3
+TODO:Validate the update form and give the appropriate messages
+TODO:Learn sql databsing or figure out if you need one, if you don't just use a csv stored to the server
+TODO:Once done with the sql, use it to write the Check and Clear classes
+TODO:For the check class, use mpld3
+
 """
 
 
 urls = (
     '/','formpage',
+    '/index','formpage',
     '/update','update',
 )
 app = web.application(urls,globals())
@@ -25,14 +26,14 @@ class formpage:
         ) #initializing a form with these attributes
  
     def GET(self):                
-        return render.form(self.simpleForm)
+        return render.index(self.simpleForm)
  
     def POST(self):
         formInput = web.input()
-        if formInput.options == 'Check':
+        if formInput.options == 'Update':
             return render.updateForm(7,update().grades.keys())  
 	#write classes for these later
-	elif formInput.options == 'Update':
+	elif formInput.options == 'Check':
 	    pass 
 	elif formInput.options == 'Clear':
 	    return formInput
@@ -48,7 +49,18 @@ class update:
     def POST(self):
         data = web.input(score=[])
 	scores = data.score #gets the array of scores
-	gpa = self.calculate(scores)
-        return render.results(gpa)  
+	print scores
+	if '' in scores:
+            return render.updateForm(7,self.grades.keys(),"Please fill in all fields")
+	else:
+            count=0
+	    for score in scores:
+	        if score not in self.grades.keys():
+		    count +=1
+	    if count>0:
+	        return render.updateForm(7,self.grades.keys(),'One or more of the scores were invalid')
+	    else:
+	        gpa = self.calculate(scores)
+                return render.results(gpa)  
 if __name__=='__main__':
     app.run()
