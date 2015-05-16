34892baccea67ce6a9a8b5685e05f1dc115a29ff
41562 <kanhaprasad546@gmail.com>
2015-05-16 19:17:41 +0700

    Graphics still to come

--- code.py
+++ code.py
@@ -2,8 +2,10 @@
 import web
 import datetime
 import csv 
+import matplotlib.pyplot as plt
+import mpld3
+import pandas as pd
 """
-Have used a static csv to store progress values
 TODO major:For the check class, use mpld3 to visualize the progress
 """
 
@@ -16,11 +18,9 @@ app = web.application(urls,globals())
 render = web.template.render('templates/') #searches for files to render in templates
  
 def csvToDict():
-    with open('log.csv','r') as filename:
+    with open('log.csv') as filename:
         reader  = csv.reader(filename,delimiter=',') #fieldnames=['Date','GPA'] 
-        display = dict()
-	for row in reader:
-            display[row[0]] = row[1]
+	display = [{row[0]:row[1]} for row in reader]
         print display
         return display
 def clearCsv():
@@ -31,11 +31,11 @@ class formpage:
     def __init__(self):
         self.simpleForm = web.form.Form(
             web.form.Radio('options',['Check your current progress','Update your score log','Clear your score log']),
-            web.form.Button('submit')    
+            web.form.Button('Submit')    
         ) #initializing a form with these attributes
  
     def GET(self):                
-        return render.index(self.simpleForm)
+        return render.index(self.simpleForm,None)
  
     def POST(self):
         formInput = web.input()
@@ -47,6 +47,8 @@ class formpage:
 	elif formInput.options == 'Clear':
 	    clearCsv()
             return """<p>Progress log was cleared successfully</p> <p> <a href="/"> Return to main page</a>"""
+	else: 
+            return render.index(self.simpleForm,'Please choose an option')
 class update:
     def __init__(self):
         self.grades = {'A*':4.3,'A':4.0,'B':3.0,'C':2.0,'D':1.0,'F':0.0}
@@ -66,7 +68,6 @@ class update:
     def POST(self):
         data = web.input(score=[])
 	scores = data.score #gets the array of scores
-	print scores
 	if '' in scores:
             return render.updateForm(7,self.grades.keys(),"Please fill in all fields")
 	else:
@@ -75,16 +76,22 @@ class update:
 	        if score not in self.grades.keys():
 		    count +=1
 	    if count>0:
-	        return render.updateForm(7,self.grades.keys(),'One or more of the scores were invalid')
+	        return render.updateForm(7,self.grades.keys(),'One or more of the scores were invalid letter grades')
 	    else:
 	        gpa = self.calculate(scores)
 		date = datetime.date.today() #the date on which it is accessed
 		self.add([date,gpa]) #add log as date,gpa pair
 		display = csvToDict() #create dict to display as a table of results
-		return render.results(gpa,display,'Your updated Progress table is:')
+		return render.results(gpa,display,'Your updated Progress table is:',None)
 class check:
     def __init__(self):
         self.display =  csvToDict()
-        self.form = render.results(None,self.display,'Your progress table is:')     
+        self.form = render.results(None,self.display,'Your progress table is:',None) 
+    """def logToGraph(self):
+        df = pd.DataFrame({'2015-05-16':[3.94],'2015-06-16':[4.00]})
+	print df
+        fig = mpld3.fig_to_html(df.plot())
+        return fig"""
+        
 if __name__=='__main__':
     app.run()
